ca27091c34f82d2ade8174ec95efcc021d099dc1
Pauline Vos <pvos88@gmail.com>
2021-03-10 17:53:12 +0100

    Make Cottage burnable

--- app/src/Status.php
+++ app/src/Status.php
@@ -8,10 +8,11 @@ use MyCLabs\Enum\Enum;
 /**
  * @author Pauline Vos <pvos88@gmail.com>
  *
+ * @method static Status burned()
  * @method static Status inTact()
- * @method static bool isInTact()
  */
 final class Status extends Enum
 {
+    private const burned = 'burned';
     private const inTact = 'inTact';
 }
